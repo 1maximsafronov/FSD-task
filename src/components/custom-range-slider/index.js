4e900eb4e8c7d4faba7f805fcb71dade5dcd5838
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 13:10:56 +0700

    Добавляет кастомный рэндж слайдер

---
+++ src/components/custom-range-slider/index.js
@@ -0,0 +1,31 @@
+import './style.scss';
+import AbstractComponent from "../abstract-component";
+
+const createTemplate = (title, value) => {
+
+  return (
+    `<div class="custom-range-slider">
+      <div class="custom-range-slider__header">
+        <h3 class="custom-range-slider__title">${title}</h3>
+        <p class="custom-range-slider__value">${value}</p>
+      </div>
+      <div class="custom-range-slider__scale">
+        <div class="custom-range-slider__bar"></div>
+        <button class="custom-range-slider__toggler custom-range-slider__toggler--min" type="button"></button>
+        <button class="custom-range-slider__toggler custom-range-slider__toggler--max" type="button"></button>
+      </div>
+    </div>`
+  );
+};
+
+export default class extends AbstractComponent {
+  constructor(title = ``, value = ``) {
+    super();
+    this._label = title;
+    this._value = value;
+  }
+
+  getTemplate() {
+    return createTemplate(this._label, this._value);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,15 +4,13 @@ import {RenderPosition, render} from "./utils/render.js";
 import LoginFormComponent from "./components/login-form";
 import ButtonComponent from "./components/button";
 import CustomTextFieldComponent from "./components/custom-text-field";
-import CustomBulletListComponent from "./components/custom-bullet-list";
+import CustomRangeSliderComponent from "./components/custom-range-slider";
 
 const main = document.querySelector(`.main`);
 const loginForm = new LoginFormComponent();
 const loginBtn = new ButtonComponent(`button`, `Войти`);
 const loginField = new CustomTextFieldComponent(`Логин`, `login`, `login`);
-const list = new CustomBulletListComponent([`Нельзя с питомцами`, `Без вечеринок и мероприятий`, `Время прибытия — после 13:00,
-а выезд до 12:00`]);
-
+const range = new CustomRangeSliderComponent(`Фильтр цены`, `5 000₽ - 10 000₽`);
 
 loginField.getElement().classList.add(`login-form__text-field`);
 loginBtn.getElement().classList.add(`login-form__btn`);
@@ -21,7 +19,7 @@ loginBtn.setClickHandler(()=>{
   disableForm(loginForm.getElement());
 });
 render(loginForm.getElement(), loginField.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), list.getElement(), RenderPosition.BEFOREEND);
+render(loginForm.getElement(), range.getElement(), RenderPosition.BEFOREEND);
 render(loginForm.getElement(), loginBtn.getElement(), RenderPosition.BEFOREEND);
 
 render(main, loginForm.getElement(), RenderPosition.BEFOREEND);
