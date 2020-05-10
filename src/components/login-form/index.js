3c5dc3cf6b489beb9f0dd17db10f4607ad6e6981
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 20:00:13 +0700

    Добавляет компонент формы логина

--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -1,10 +1,50 @@
 import './style.scss';
-import {createSomeElement} from "../../utils/render.js";
+// import {disableForm} from "./utils/form-actions.js";
+import {createSomeElement, render, RenderPosition} from "../../utils/render.js";
+import CustomTextFieldComponent from "../custom-text-field";
+import CustomTitleH1Component from "../custom-title-h1";
+import ButtonComponent from "../button";
 
 const createTemplate = () => {
   return (
-    `<form class="login-form"></form>`
+    `<form class="login-form">
+      <div class="login-form__isRegistered">
+        <p class="login-form__question">Нет аккаунта на Toxin?</>
+      </div>
+    </form>`
   );
+};
+
+const renderLoginFormContent = (container) =>{
+  const loginFormTitl = new CustomTitleH1Component(`Войти`);
+  const loginField = new CustomTextFieldComponent({
+    label: `Логин`,
+    isLabelShown: 0,
+    name: `login`,
+    id: `login`,
+    placeholder: `Email`
+  });
+  const passField = new CustomTextFieldComponent({
+    label: `Пароль`,
+    isLabelShown: 0,
+    name: `pass`,
+    id: `pass`,
+    type: `password`,
+    placeholder: `Пароль`
+  });
+  const loginBtn = new ButtonComponent(`button`, `Войти`, `arrow`);
+  const createBtn = new ButtonComponent(`button`, `Создать`, `border`);
+  loginFormTitl.addClass(`login-form__title`);
+  loginField.addClass(`login-form__text-field`);
+  passField.addClass(`login-form__text-field`);
+  loginBtn.addClass(`login-form__btn`);
+  createBtn.addClass(`login-form__createBtn`);
+  render(container, loginBtn.getElement(), RenderPosition.AFTERBEGIN);
+  render(container, passField.getElement(), RenderPosition.AFTERBEGIN);
+  render(container, loginField.getElement(), RenderPosition.AFTERBEGIN);
+  render(container, loginFormTitl.getElement(), RenderPosition.AFTERBEGIN);
+  const bottomContainer = container.querySelector(`.login-form__isRegistered`);
+  render(bottomContainer, createBtn.getElement(), RenderPosition.BEFOREEND);
 };
 
 export default class {
@@ -18,10 +58,15 @@ export default class {
 
   getElement() {
     if (!this._element) {
-      this._element = createSomeElement(this.getTemplate());
+      this._renderContent();
     }
 
     return this._element;
+  }
+
+  _renderContent() {
+    this._element = createSomeElement(this.getTemplate());
+    renderLoginFormContent(this._element);
   }
 
   removeElement() {
--- src/main.js
+++ src/main.js
@@ -1,49 +1,10 @@
 import './sass/style.scss';
-import {disableForm} from "./utils/form-actions.js";
 import {RenderPosition, render} from "./utils/render.js";
+// import SiteNavComponent from "./components/site-nav";
+// import UserNavComponent from "./components/user-nav";
 import LoginFormComponent from "./components/login-form";
-import ButtonComponent from "./components/button";
-import CustomTextFieldComponent from "./components/custom-text-field";
-import CustomTitleH1Component from "./components/custom-title-h1";
-import SiteNavComponent from "./components/site-nav";
-import UserNavComponent from "./components/user-nav";
 const main = document.querySelector(`.main`);
-const pageHeader = document.querySelector(`header`);
 
-const siteNav = new SiteNavComponent();
-const userNav = new UserNavComponent(`logined`);
 const loginForm = new LoginFormComponent();
-const title = new CustomTitleH1Component(`Войти`);
-const loginField = new CustomTextFieldComponent({
-  label: `Логин`,
-  isLabelShown: 0,
-  name: `login`,
-  id: `login`,
-  placeholder: `Email`
-});
-const passField = new CustomTextFieldComponent({
-  label: `Пароль`,
-  isLabelShown: 0,
-  name: `pass`,
-  id: `pass`,
-  type: `password`,
-  placeholder: `Пароль`
-});
-const loginBtn = new ButtonComponent(`button`, `Войти`, `arrow`);
-
-title.addClass(`login-form__title`);
-loginField.addClass(`login-form__text-field`);
-passField.addClass(`login-form__text-field`);
-loginBtn.addClass(`login-form__btn`);
-
-loginBtn.setClickHandler(()=>{
-  disableForm(loginForm.getElement());
-});
-render(pageHeader, siteNav.getElement(), RenderPosition.BEFOREEND);
-render(pageHeader, userNav.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), title.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), loginField.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), passField.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), loginBtn.getElement(), RenderPosition.BEFOREEND);
 
 render(main, loginForm.getElement(), RenderPosition.BEFOREEND);
