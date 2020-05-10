f49b1e31b88dfdd8449d082044fb7dac9d7d9a17
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 14:38:23 +0700

    Добавляет разворачивающийся список чекбоксов

---
+++ src/components/expandable-checkbox-list/index.js
@@ -0,0 +1,63 @@
+import './style.scss';
+import {render, createSomeElement, RenderPosition} from "../../utils/render.js";
+
+import AbstractComponent from "../abstract-component";
+import CustomCheckboxComponent from "../custom-checkbox";
+
+const createItemTemplate = () => {
+  return (
+    `<li class="expandable-checkbox-list__item"></li>`
+  );
+};
+
+const createListElements = (elements, container) => {
+  const listContainer = container.querySelector(`.expandable-checkbox-list__list`);
+  elements.forEach((item) => {
+    let listItem = createSomeElement(createItemTemplate());
+    const newCheckbox = new CustomCheckboxComponent(item.label, item.name, item.id);
+    render(listItem, newCheckbox.getElement(), RenderPosition.BEFOREEND);
+    render(listContainer, listItem, RenderPosition.BEFOREEND);
+  });
+
+  container.querySelector(`.expandable-checkbox-list__toggler`).addEventListener(`click`, ()=>{
+    container.classList.toggle(`expandable-checkbox-list--closed`);
+  });
+
+  return container;
+
+
+};
+
+const createTemplate = (title) => {
+
+
+  return (
+    `<div class="expandable-checkbox-list expandable-checkbox-list--closed">
+      <div class="expandable-checkbox-list__toggler" type="button">${title}</div>
+      <ul class="expandable-checkbox-list__list">
+      </ul>
+    </dvi>`
+  );
+};
+
+export default class extends AbstractComponent {
+  constructor(title, elements) {
+    super();
+    this._title = title;
+    this._elements = elements;
+  }
+
+  getTemplate() {
+    return createTemplate(this._title);
+  }
+
+  getElement() {
+    this._element = createSomeElement(this.getTemplate());
+    this.renderList();
+    return this._element;
+  }
+
+  renderList() {
+    this._element = createListElements(this._elements, this._element);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,12 +4,31 @@ import {RenderPosition, render} from "./utils/render.js";
 import LoginFormComponent from "./components/login-form";
 import ButtonComponent from "./components/button";
 import CustomTextFieldComponent from "./components/custom-text-field";
+import CheckboxListComponent from "./components/expandable-checkbox-list";
 
 const main = document.querySelector(`.main`);
 const loginForm = new LoginFormComponent();
 const loginBtn = new ButtonComponent(`button`, `Войти`);
 const loginField = new CustomTextFieldComponent(`Логин`, `login`, `login`);
 const loginField2 = new CustomTextFieldComponent(`Пароль`, `password`, `password`, `password`);
+
+const elements = [
+  {
+    label: `Wi-Fi`,
+    name: `firs`,
+    id: `firs`,
+  },
+  {
+    label: `Холодильник`,
+    name: `second`,
+    id: `second`,
+  }, {
+    label: `Балкон`,
+    name: `third`,
+    id: `third`,
+  }
+];
+const checkboxList = new CheckboxListComponent(`Выберете функции`, elements);
 loginField.addClass(`login-form__text-field`);
 loginField2.addClass(`login-form__text-field`);
 loginBtn.addClass(`login-form__btn`);
@@ -19,6 +38,7 @@ loginBtn.setClickHandler(()=>{
 });
 render(loginForm.getElement(), loginField.getElement(), RenderPosition.BEFOREEND);
 render(loginForm.getElement(), loginField2.getElement(), RenderPosition.BEFOREEND);
+render(loginForm.getElement(), checkboxList.getElement(), RenderPosition.BEFOREEND);
 render(loginForm.getElement(), loginBtn.getElement(), RenderPosition.BEFOREEND);
 
 render(main, loginForm.getElement(), RenderPosition.BEFOREEND);
