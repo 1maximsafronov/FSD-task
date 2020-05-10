41d8fda6334539c56b10923d9b763872f13a5376
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 15:38:47 +0700

    Исправляет внешние отступы у всех заголовков. Исправляет задание второстеменного вида шрифта. Добавляет заголовок на страницу для теста

--- src/components/custom-text-field/index.js
+++ src/components/custom-text-field/index.js
@@ -1,25 +1,27 @@
 import './style.scss';
 import AbstractComponent from "../abstract-component";
 
-const createTemplate = (label, name, id, type) => {
+const createTemplate = (label, isLabelShown, name, id, type, placeholder) => {
   return (
     `<p class="custom-text-field">
-    <label for="${id}">${label}</label>
-    <input type="${type}" name="${name}" id="${id}">
+    <label for="${id}" class="${isLabelShown ? `` : `visually-hidden`}">${label}</label>
+    <input type="${type}" name="${name}" id="${id}" placeholder="${placeholder}">
     </p>`
   );
 };
 
 export default class extends AbstractComponent {
-  constructor(label, name, id, type = `text`) {
+  constructor({label, isLabelShown, name, id, type = `text`, placeholder = ``}) {
     super();
     this._label = label;
+    this._isLabelShown = isLabelShown;
     this._name = name;
     this._id = id;
     this._type = type;
+    this._placeholder = placeholder;
   }
 
   getTemplate() {
-    return createTemplate(this._label, this._name, this._id, this._type);
+    return createTemplate(this._label, this._isLabelShown, this._name, this._id, this._type, this._placeholder);
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,35 +4,32 @@ import {RenderPosition, render} from "./utils/render.js";
 import LoginFormComponent from "./components/login-form";
 import ButtonComponent from "./components/button";
 import CustomTextFieldComponent from "./components/custom-text-field";
-import CheckboxListComponent from "./components/expandable-checkbox-list";
-import CustomTitleH1Component from "./components/custom-title-h3";
+import CustomTitleH1Component from "./components/custom-title-h1";
+
 const main = document.querySelector(`.main`);
+
 const loginForm = new LoginFormComponent();
+const title = new CustomTitleH1Component(`Войти`);
+const loginField = new CustomTextFieldComponent({
+  label: `Логин`,
+  isLabelShown: 0,
+  name: `login`,
+  id: `login`,
+  placeholder: `Email`
+});
+const passField = new CustomTextFieldComponent({
+  label: `Пароль`,
+  isLabelShown: 0,
+  name: `pass`,
+  id: `pass`,
+  type: `password`,
+  placeholder: `Пароль`
+});
 const loginBtn = new ButtonComponent(`button`, `Войти`);
-const loginField = new CustomTextFieldComponent(`Логин`, `login`, `login`);
-const loginField2 = new CustomTextFieldComponent(`Пароль`, `password`, `password`, `password`);
 
-const title = new CustomTitleH1Component(`Форма входа`);
-
-const elements = [
-  {
-    label: `Wi-Fi`,
-    name: `firs`,
-    id: `firs`,
-  },
-  {
-    label: `Холодильник`,
-    name: `second`,
-    id: `second`,
-  }, {
-    label: `Балкон`,
-    name: `third`,
-    id: `third`,
-  }
-];
-const checkboxList = new CheckboxListComponent(`Выберете функции`, elements);
+title.addClass(`login-form__title`);
 loginField.addClass(`login-form__text-field`);
-loginField2.addClass(`login-form__text-field`);
+passField.addClass(`login-form__text-field`);
 loginBtn.addClass(`login-form__btn`);
 
 loginBtn.setClickHandler(()=>{
@@ -40,8 +37,7 @@ loginBtn.setClickHandler(()=>{
 });
 render(loginForm.getElement(), title.getElement(), RenderPosition.BEFOREEND);
 render(loginForm.getElement(), loginField.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), loginField2.getElement(), RenderPosition.BEFOREEND);
-render(loginForm.getElement(), checkboxList.getElement(), RenderPosition.BEFOREEND);
+render(loginForm.getElement(), passField.getElement(), RenderPosition.BEFOREEND);
 render(loginForm.getElement(), loginBtn.getElement(), RenderPosition.BEFOREEND);
 
 render(main, loginForm.getElement(), RenderPosition.BEFOREEND);
