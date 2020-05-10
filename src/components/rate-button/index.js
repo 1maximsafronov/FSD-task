4b392cf725af2764f5b6c8069e1f681ba8a9bd05
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 22:18:06 +0700

    Исправляет кнопку рейтинга. задавая ей номер сета звёзд, чтобы был уникальный набор

--- src/components/rate-button/index.js
+++ src/components/rate-button/index.js
@@ -2,36 +2,36 @@ import './style.scss';
 import {createSomeElement} from "../../utils/render.js";
 import AbstractComponent from "../abstract-component";
 
-const createTemplate = () => {
+const createTemplate = (setNumber) => {
   return (
     `<ul class="rate-button">
       <li class="rate-button__item">
-        <input class="visually-hidden" id="1-star" type="checkbox" name="rate" value="1">
-        <label class="rate-button__star" for="1-star">
+        <input class="visually-hidden" id="set-number-${setNumber}-star-1" type="checkbox" name="rate" value="1">
+        <label class="rate-button__star" for="set-number-${setNumber}-star-1">
           <span class="visually-hidden">1 звезда</span>
         </label>
       </li>
       <li class="rate-button__item">
-        <input class="visually-hidden" id="2-star" type="checkbox" name="rate" value="2">
-        <label class="rate-button__star" for="2-star">
+        <input class="visually-hidden" id="set-number-${setNumber}-star-2" type="checkbox" name="rate" value="2">
+        <label class="rate-button__star" for="set-number-${setNumber}-star-2">
           <span class="visually-hidden">2 звезды</span>
         </label>
       </li>
       <li class="rate-button__item">
-        <input class="visually-hidden" id="3-star" type="checkbox" name="rate" value="3">
-        <label class="rate-button__star" for="3-star">
+        <input class="visually-hidden" id="set-number-${setNumber}-star-3" type="checkbox" name="rate" value="3">
+        <label class="rate-button__star" for="set-number-${setNumber}-star-3">
           <span class="visually-hidden">3 звезды</span>
         </label>
       </li>
       <li class="rate-button__item">
-        <input class="visually-hidden" id="4-star" type="checkbox" name="rate" value="4">
-        <label class="rate-button__star" for="4-star">
+        <input class="visually-hidden" id="set-number-${setNumber}-star-4" type="checkbox" name="rate" value="4">
+        <label class="rate-button__star" for="set-number-${setNumber}-star-4">
           <span class="visually-hidden">4 звезды</span>
         </label>
       </li>
       <li class="rate-button__item">
-        <input class="visually-hidden" id="5-star" type="checkbox" name="rate" value="5">
-        <label class="rate-button__star" for="5-star">
+        <input class="visually-hidden" id="set-number-${setNumber}-star-5" type="checkbox" name="rate" value="5">
+        <label class="rate-button__star" for="set-number-${setNumber}-star-5">
           <span class="visually-hidden">5 звезд</span>
         </label>
       </li>
@@ -65,12 +65,13 @@ const setHandlers = (rateBtn)=>{
 };
 
 export default class extends AbstractComponent {
-  constructor() {
+  constructor(setNumber) {
     super();
+    this._setNumber = setNumber;
   }
 
   getTemplate() {
-    return createTemplate();
+    return createTemplate(this._setNumber);
   }
   getElement() {
     if (!this._element) {
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import {RenderPosition, render} from "./utils/render.js";
 // import SiteNavComponent from "./components/site-nav";
 // import UserNavComponent from "./components/user-nav";
 const main = document.querySelector(`.main`);
-// render(main, rateBtn.getElement(), RenderPosition.BEFOREEND);
+
+// render(main, rateButton1.getElement(), RenderPosition.BEFOREEND);
