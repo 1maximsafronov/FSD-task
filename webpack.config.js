43ef27aaddc99664f69e177d00d8f7735829d851
1maximsafronov <1maximsafronov@gmail.com>
2020-05-10 11:56:04 +0700

    Добавляет модуль file-loader для webpack для загрузки изображений вместе с компонентом. Перемещает изображение лайка в папку с компонентом

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader',
+        ],
+      },
     ],
   },
 };
