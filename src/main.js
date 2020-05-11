97c03ca12a4593b1c9052a5bc4b70cd51392e3e2
1maximsafronov <1maximsafronov@gmail.com>
2020-05-11 10:17:34 +0700

    Добавляет Копирайт виджет для футера

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './sass/style.scss';
 import {RenderPosition, render} from "./utils/render.js";
 // import SiteNavComponent from "./components/site-nav";
 // import UserNavComponent from "./components/user-nav";
-import SocialComponent from "./components/social";
+import SocialComponent from "./components/copyright";
 const main = document.querySelector(`.main`);
 const block = new SocialComponent();
 render(main, block.getElement(), RenderPosition.BEFOREEND);
