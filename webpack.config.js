7586d1708bb672606591c4862f9bed14c7f42642
1maximsafronov <1maximsafronov@gmail.com>
2020-05-17 19:57:58 +0700

    Отделяет компонент кастомных полей ввода

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,116 @@
-const path = require('path');
-const fs = require('fs');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require(`path`);
+const fs = require(`fs`);
+const MiniCssExtractPlugin = require(`mini-css-extract-plugin`);
+const CopyWebpackPlugin = require(`copy-webpack-plugin`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+const {
+  CleanWebpackPlugin
+} = require(`clean-webpack-plugin`);
+
 const PATHS = {
-  src: path.join(__dirname, './src'),
-  build: path.join(__dirname, './build')
+  src: path.join(__dirname, `./src`),
+  build: path.join(__dirname, `./build`)
 };
 
-const PAGES_DIR = PATHS.src + '/pug/';
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+const PAGES_DIR = PATHS.src + `/pug/`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(`.pug`));
 
 module.exports = {
   entry: {
     app: PATHS.src
   },
   output: {
-    filename: 'js/main.js',
+    filename: `js/main.js`,
     path: PATHS.build,
-    publicPath: '/'
+    publicPath: `/`
   },
-  devtool: 'source-map',
+  devtool: `source-map`,
   devServer: {
     contentBase: PATHS.build,
     overlay: true
   },
   module: {
-    rules:  [
-      {
+    rules: [{
       test: /\.pug$/,
-      loader: 'pug-loader'
+      loader: `pug-loader`
     },
     {
       test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
+      loader: `babel-loader`,
+      exclude: `/node_modules/`
     },
     {
       test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      exclude: '/src/fonts/',
-      options: { name: '[name].[ext]'}
+      loader: `file-loader`,
+      exclude: `/src/fonts/`,
+      options: {
+        name: `[name].[ext]`
+      }
     },
     {
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      exclude: '/src/img/',
+      loader: `file-loader`,
+      exclude: `/src/img/`,
       options: {
-        name: '[name].[ext]'
+        name: `[name].[ext]`
       }
     },
     {
       test: /\.scss$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: MiniCssExtractPlugin.loader,
-        },
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: {
-              path: './postcss.config.js' }
-          }
-        },
-        {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
+      use: [{
+        loader: `style-loader`
+      },
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+        loader: `css-loader`,
+        options: {
+          sourceMap: true
         }
+      },
+      {
+        loader: `postcss-loader`,
+        options: {
+          sourceMap: true,
+          config: {
+            path: `./postcss.config.js`
+          }
+        }
+      },
+      {
+        loader: `sass-loader`,
+        options: {
+          sourceMap: true
+        }
+      }
       ]
     },
     {
       test: /\.css$/,
-      use: [
-        {
-          loader: 'style-loader',
-        }, {
-          loader: MiniCssExtractPlugin.loader,
-        }, {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            config: { path: PATHS.src + '/js/postcss.config.js' }
+      use: [{
+        loader: `style-loader`
+      },
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+        loader: `css-loader`,
+        options: {
+          sourceMap: true
+        }
+      },
+      {
+        loader: `postcss-loader`,
+        options: {
+          sourceMap: true,
+          config: {
+            path: `./postcss.config.js`
           }
         }
+      }
       ]
     }
     ]
@@ -102,20 +118,27 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/style.css',
+      filename: `css/style.css`
     }),
+    new CopyWebpackPlugin([{
+      from: PATHS.src + `/img`,
+      to: `img`
+    },
+    {
+      from: PATHS.src + `/fonts`,
+      to: `fonts`
+    }
+    ]),
     // new HtmlWebpackPlugin({
     //   hash: false,
     //   template: PATHS.src + '/index.html',
     //   filename: 'index.html',
     // }),
-    new CopyWebpackPlugin([
-      { from: PATHS.src + '/img', to: 'img' },
-      { from: PATHS.src + '/fonts', to: 'fonts' },
-    ]),
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/${page}`,
-      filename: `./${page.replace(/\.pug/,'.html')}`
-    }))
-  ],
-}
+    ...PAGES.map((page) =>
+      new HtmlWebpackPlugin({
+        template: `${PAGES_DIR}/${page}`,
+        filename: `./${page.replace(/\.pug/, `.html`)}`
+      })
+    )
+  ]
+};
